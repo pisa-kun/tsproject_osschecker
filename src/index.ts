f98be869bf14deacae59b38a80a624ec2759be4e
pisa <pisa0401mst@gmail.com>
2022-10-19 00:18:26 +0900

    MinorとMajorの連想配列に代入

--- src/index.ts
+++ src/index.ts
@@ -22,6 +22,18 @@ const seekRootDir = "C:\\Develop\\typescript\\programming_ts_oreilly";
 const targetFileName = "package.json"
 
 /**
+ * マイナーバージョンを格納する連想配列
+ * @interface {OssVersionArray}
+ */
+const ossMinorVer: OssVersionArray = {}
+
+/**
+ * メジャーバージョンを格納する連想配列
+ * @interface {OssVersionArray}
+ */
+const ossMajorVer: OssVersionArray = {}
+
+/**
  * 検索対象のファイル名を全て出力する
  * https://kuroeveryday.blogspot.com/2018/03/recursive-directory-search-with-nodejs.html
  * @param {string} dir 検索するディレクトリのパス
@@ -45,11 +57,39 @@ const getAllFiles = (dir: string): string[] => {
 
 const main = () => {
     const p = process.cwd();
+
+    /**
+     * 連想配列内のメジャー、マイナーバージョンを更新する
+     * @param {OssVersionArray} minor マイナーバージョンを格納する連想配列
+     * @param {OssVersionArray} major メジャーバージョンを格納する連想配列
+     * @param {string} name oss名
+     * @param {string} ver バージョン(x.x.x)
+     */
+    const setMajorAndMinorOss = (minor: OssVersionArray, major: OssVersionArray, name: string, ver: string) => {
+        if(!minor[name] || minor[name] > ver) {
+            minor[name] = ver;
+        }
+        
+        if(!major[name] || major[name] < ver){
+            major[name] = ver
+        }
+    }
+
     getAllFiles(p).filter(file => path.basename(file) === targetFileName)
     .forEach(file => {
         const from_json = JSON.parse(fs.readFileSync(file, 'utf8'));
-        console.log(from_json["name"], ":", from_json["version"]);
+
+        const name = from_json["name"];
+        const version = from_json["version"];
+        if(name && version) {
+            setMajorAndMinorOss(ossMinorVer, ossMajorVer, name, version);
+        }
     });
+    
+    console.log("oss, Minor X.X.X, Major X.X.X")
+    for(const oss in ossMinorVer){
+        console.log(`${oss}, ${ossMinorVer[oss]}, ${ossMajorVer[oss]}`);
+    }
 }
 
 main();
