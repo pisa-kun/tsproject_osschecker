33a7461563a557b7e512f1200870329386e23618
pisa <pisa0401mst@gmail.com>
2022-10-20 20:50:10 +0900

    format適用

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ import * as path from "path";
  * @type {string}
  */
 interface OssVersionArray {
-    [oss: string]: string;
+  [oss: string]: string;
 }
 
 /**
@@ -14,38 +14,38 @@ interface OssVersionArray {
  * @param {string} X.X.X表記の文字列
  * TODO: 2.0.0-next.3 のような値への対応
  */
-class SemanticVersion{
-    constructor(private readonly value: string){
-        if(!value){
-            throw new Error("value is undefined");
-        }
-        if(!(/^\d+.\d+.\d+$/).test(value)){
-            throw new Error(`value(${value}) is not semantic pattern`);
-        }
+class SemanticVersion {
+  constructor(private readonly value: string) {
+    if (!value) {
+      throw new Error("value is undefined");
     }
+    if (!/^\d+.\d+.\d+$/.test(value)) {
+      throw new Error(`value(${value}) is not semantic pattern`);
+    }
+  }
 
-    getValue(): string {
-        return this.value;
-    }
+  getValue(): string {
+    return this.value;
+  }
 }
 
 /**
  * 取得したいファイル名
  * @type {string}
  */
-const targetFileName = "package.json"
+const targetFileName = "package.json";
 
 /**
  * マイナーバージョンを格納する連想配列
  * @interface {OssVersionArray}
  */
-const ossMinorVer: OssVersionArray = {}
+const ossMinorVer: OssVersionArray = {};
 
 /**
  * メジャーバージョンを格納する連想配列
  * @interface {OssVersionArray}
  */
-const ossMajorVer: OssVersionArray = {}
+const ossMajorVer: OssVersionArray = {};
 
 /**
  * 検索対象のファイル名を全て出力する
@@ -54,20 +54,20 @@ const ossMajorVer: OssVersionArray = {}
  * @return {Array<string>} ファイルのパスのリスト
  */
 const getAllFiles = (dir: string): string[] => {
-    const strArray: string[] = [];
+  const strArray: string[] = [];
 
-    const filenames = fs.readdirSync(dir);
-    filenames.forEach((filename) => {
-        const fullPath = path.join(dir, filename);
-        const stats = fs.statSync(fullPath);
-        if(stats.isFile()) {
-            strArray.push(fullPath);
-        }else if(stats.isDirectory()){
-            strArray.push(...getAllFiles(fullPath));
-        }
-    });
-    return strArray;
-}
+  const filenames = fs.readdirSync(dir);
+  filenames.forEach((filename) => {
+    const fullPath = path.join(dir, filename);
+    const stats = fs.statSync(fullPath);
+    if (stats.isFile()) {
+      strArray.push(fullPath);
+    } else if (stats.isDirectory()) {
+      strArray.push(...getAllFiles(fullPath));
+    }
+  });
+  return strArray;
+};
 
 /**
  * 連想配列内のメジャー、マイナーバージョンを更新する
@@ -76,39 +76,45 @@ const getAllFiles = (dir: string): string[] => {
  * @param {string} name oss名
  * @param {SemanticVersion} ver バージョン(x.x.x)
  */
-const setMajorAndMinorOss = (minor: OssVersionArray, major: OssVersionArray, name: string, ver: SemanticVersion) => {
-    if(!minor[name] || minor[name] > ver.getValue()) {
-        minor[name] = ver.getValue();
-    }
-    
-    if(!major[name] || major[name] < ver.getValue()){
-        major[name] = ver.getValue()
-    }
-}
+const setMajorAndMinorOss = (
+  minor: OssVersionArray,
+  major: OssVersionArray,
+  name: string,
+  ver: SemanticVersion
+) => {
+  if (!minor[name] || minor[name] > ver.getValue()) {
+    minor[name] = ver.getValue();
+  }
+
+  if (!major[name] || major[name] < ver.getValue()) {
+    major[name] = ver.getValue();
+  }
+};
 
 const main = () => {
-    if(process.argv.length <= 1 || !fs.statSync(process.argv[2]).isDirectory()){
-        throw new Error("please set argument directory path");
-    }
-    const dir = process.argv[2];
+  if (process.argv.length <= 1 || !fs.statSync(process.argv[2]).isDirectory()) {
+    throw new Error("please set argument directory path");
+  }
+  const dir = process.argv[2];
 
-    getAllFiles(dir).filter(file => path.basename(file) === targetFileName)
-    .forEach(file => {
-        const from_json = JSON.parse(fs.readFileSync(file, 'utf8'));
+  getAllFiles(dir)
+    .filter((file) => path.basename(file) === targetFileName)
+    .forEach((file) => {
+      const from_json = JSON.parse(fs.readFileSync(file, "utf8"));
 
-        const name = from_json["name"];
-        const version = from_json["version"];
-        // package.jsonにversionキーがないものが多かったのでキーがない場合skip
-        const semVer = version ? new SemanticVersion(version) : undefined;
-        if(name && semVer) {
-            setMajorAndMinorOss(ossMinorVer, ossMajorVer, name, semVer);
-        }
+      const name = from_json["name"];
+      const version = from_json["version"];
+      // package.jsonにversionキーがないものが多かったのでキーがない場合skip
+      const semVer = version ? new SemanticVersion(version) : undefined;
+      if (name && semVer) {
+        setMajorAndMinorOss(ossMinorVer, ossMajorVer, name, semVer);
+      }
     });
-    
-    console.log("oss, Minor X.X.X, Major X.X.X");
-    for(const oss in ossMinorVer){
-        console.log(`${oss}, ${ossMinorVer[oss]}, ${ossMajorVer[oss]}`);
-    }
-}
+
+  console.log("oss, Minor X.X.X, Major X.X.X");
+  for (const oss in ossMinorVer) {
+    console.log(`${oss}, ${ossMinorVer[oss]}, ${ossMajorVer[oss]}`);
+  }
+};
 
 main();
