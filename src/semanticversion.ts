cfefbd685b649a09eb867bc33302b6b63f92208f
pisa <pisa0401mst@gmail.com>
2022-10-21 07:40:37 +0900

    major,minor,patchをプロパティに

--- src/semanticversion.ts
+++ src/semanticversion.ts
@@ -3,6 +3,11 @@
  * @param {string} X.X.X表記の文字列
  */
  export class SemanticVersion {
+    private readonly major: number;
+    private readonly minor: number;
+    private readonly patch: number;
+    //private readonly pre: string;
+    
     constructor(private readonly value: string) {
       if (!value) {
         throw new Error("value is undefined");
@@ -10,6 +15,11 @@
       if (!/^\d+.\d+.\d+(-..*)?$/.test(value)) {
         throw new Error(`value(${value}) is not semantic pattern`);
       }
+      const separate = value.split(/[.-]/);
+      const[major, minor, patch, _] = [...separate];
+      this.major = Number(major);
+      this.minor = Number(minor);
+      this.patch = Number(patch);
     }
   
     getValue(): string {
