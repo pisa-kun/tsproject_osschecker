373cfc6997841798be8342bc4efc46ef4c244203
pisa <pisa0401mst@gmail.com>
2022-10-22 09:40:40 +0900

    semanticVersionの見直しとformatの範囲をsrc/*.tsへ

--- src/semanticversion.test.ts
+++ src/semanticversion.test.ts
@@ -1,18 +1,40 @@
 import { SemanticVersion } from "./semanticversion";
 
-test('create instance', () => {
-    new SemanticVersion("1.1.1");
-    new SemanticVersion("1.2.1");
-    new SemanticVersion("1.1.11");
-    new SemanticVersion("11.1.1");
-    new SemanticVersion("1.11.1");
-    new SemanticVersion("1.1.1-β");
-    new SemanticVersion("1.1.1-beta");
-    new SemanticVersion("1.1.1-beta-beta"); 
+test("create instance", () => {
+  {
+    const ver = new SemanticVersion("1.1.1");
+    expect(ver.getMajor()).toBe(1);
+    expect(ver.getMinor()).toBe(1);
+    expect(ver.getPatch()).toBe(1);
+  }
+  {
+    const ver = new SemanticVersion("1.2.1");
+    expect(ver.getMajor()).toBe(1);
+    expect(ver.getMinor()).toBe(2);
+    expect(ver.getPatch()).toBe(1);
+  }
+  {
+    const ver = new SemanticVersion("1.1.11");
+    expect(ver.getMajor()).toBe(1);
+    expect(ver.getMinor()).toBe(1);
+    expect(ver.getPatch()).toBe(11);
+  }
+  {
+    const ver = new SemanticVersion("1.1.1-β");
+    expect(ver.getMajor()).toBe(1);
+    expect(ver.getMinor()).toBe(1);
+    expect(ver.getPatch()).toBe(1);
+  }
 });
 
 test("throw assert", () => {
-    expect(() => {new SemanticVersion("1.1.1-")}).toThrow();
-    expect(() => {new SemanticVersion("1.1.1beta-beta")}).toThrow();
-    expect(() => {new SemanticVersion(".1.1-")}).toThrow();
-})
+  expect(() => {
+    new SemanticVersion("1.1.1-");
+  }).toThrow();
+  expect(() => {
+    new SemanticVersion("1.1.1beta-beta");
+  }).toThrow();
+  expect(() => {
+    new SemanticVersion(".1.1-");
+  }).toThrow();
+});
--- src/semanticversion.ts
+++ src/semanticversion.ts
@@ -2,27 +2,39 @@
  * セマンティックバージョンを表すバリュークラス
  * @param {string} X.X.X表記の文字列
  */
- export class SemanticVersion {
-    private readonly major: number;
-    private readonly minor: number;
-    private readonly patch: number;
-    //private readonly pre: string;
-    
-    constructor(private readonly value: string) {
-      if (!value) {
-        throw new Error("value is undefined");
-      }
-      if (!/^\d+.\d+.\d+(-..*)?$/.test(value)) {
-        throw new Error(`value(${value}) is not semantic pattern`);
-      }
-      const separate = value.split(/[.-]/);
-      const[major, minor, patch, _] = [...separate];
-      this.major = Number(major);
-      this.minor = Number(minor);
-      this.patch = Number(patch);
+export class SemanticVersion {
+  private readonly major: number;
+  private readonly minor: number;
+  private readonly patch: number;
+  //private readonly pre: string;
+
+  constructor(private readonly value: string) {
+    if (!value) {
+      throw new Error("value is undefined");
     }
-  
-    getValue(): string {
-      return this.value;
+    if (!/^\d+.\d+.\d+(-..*)?$/.test(value)) {
+      throw new Error(`value(${value}) is not semantic pattern`);
     }
+    const separate = value.split(/[.-]/);
+    const [major, minor, patch, _] = [...separate];
+    this.major = Number(major);
+    this.minor = Number(minor);
+    this.patch = Number(patch);
   }
+  /**
+   * バージョンを文字列で返す
+   */
+  getValue = (): string => this.value;
+  /**
+   * メジャーバージョンを数字で返す
+   */
+  getMajor = (): number => this.major;
+  /**
+   * マイナーバージョンを数字で返す
+   */
+  getMinor = (): number => this.minor;
+  /**
+   * パッチバージョンを数字で返す
+   */
+  getPatch = (): number => this.patch;
+}
